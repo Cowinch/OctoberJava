405a653cccd988923adbd8ec2d7805d3f4d12913
Cole Winchester <cowinch@gmail.com>
2022-09-26 17:26:39 -0500

    PM lecture

--- fundamentals/monday-lecture/messages/VoiceMessageTest.java
+++ fundamentals/monday-lecture/messages/VoiceMessageTest.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class VoiceMessageTest {
     public static void main(String[] args) {
 
@@ -6,5 +8,22 @@ public class VoiceMessageTest {
         System.out.print(messageUtil.greeting("Bobby"));
 
         System.out.println(messageUtil.greeting(23));
+
+        int[] messagesPerDay= new int[5];
+        messagesPerDay[0]=8;
+        messagesPerDay[1]=6;
+        messagesPerDay[2]=10;
+        System.out.println(messagesPerDay);
+
+        String[] messages={"asdf"," f sd","dfg sdfgd dv"};
+        messageUtil.printAllMessages(messages);
+
+        ArrayList<String> todoList = new ArrayList<String>();
+        todoList.add("Finish Alfred Bot");
+        todoList.add("Finish Cafe Business Logic");
+        todoList.add("Read ahead for OOP");
+
+        messageUtil.printAllMessages(todoList);
     }
+
 }
--- fundamentals/monday-lecture/messages/VoiceMessageUtil.java
+++ fundamentals/monday-lecture/messages/VoiceMessageUtil.java
@@ -1,4 +1,5 @@
 import java.util.Date;
+import java.util.ArrayList;
 public class VoiceMessageUtil {
     public String greeting(String name){
         return "Hello, " + name;
@@ -9,5 +10,28 @@ public class VoiceMessageUtil {
     public void getTodaysDate(){
         Date today=new Date();
         System.out.println("Hello, the current date is "+ today);
+    }
+
+    public int countTotalMessages(int[] messageCount){
+        int output=0;
+        for(int i=0; i< messageCount.length;i++){
+            output+=messageCount[i];
+        }
+        return output;
+    }
+
+    public void printAllMessages(String[] messages){
+        // for(int i=0;i<messages.length;i++){
+        //     System.out.println(messages[i]);
+        // }
+        for(String message : messages){
+            System.out.println(message);
+        }
+    }
+
+    public void printAllMessages(ArrayList<String> messages){
+        for(int i=0;i<messages.size();i++){
+            System.out.println(messages.get(i));
+        }
     }
 }
